60409756cc6db1faec3dbed8b09d7e6d48c11544
ogabek0423 <ogabek0423@gmail.com>
2026-01-01 21:55:43 +0500

    ilk holat

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace PaymentLibrary
+{
+    public class LoanCalculator
+    {
+        public static decimal CalculateMonthlyPayment(
+            decimal principal,
+            double annualInterestRate,
+            int termInYears,
+            decimal additionalFee = 0)
+        {
+            if (principal <= 0)
+                throw new ArgumentException("Asosiy qarz musbat bolishi kerak");
+            if (annualInterestRate < 0)
+                throw new ArgumentException("Foiz stavkasi manfiy bolmasligi kerak");
+            if (termInYears <= 0)
+                throw new ArgumentException("Muddat musbat bolishi kerak");
+            if (additionalFee < 0)
+                throw new ArgumentException("Qoshimcha komissiya manfiy bo‘lmasligi kerak");
+
+            double monthlyRate = annualInterestRate / 100 / 12;
+            int totalMonths = termInYears * 12;
+
+            decimal basePayment = principal * (decimal)(monthlyRate * Math.Pow(1 + monthlyRate, totalMonths)) /
+                                  (decimal)(Math.Pow(1 + monthlyRate, totalMonths) - 1);
+
+            return Math.Round(basePayment + additionalFee, 2);
+        }
+    }
+}
